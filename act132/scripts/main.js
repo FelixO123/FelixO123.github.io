89f14b2d477c80855915a208b995d881a7fee20e
Your Name <you@example.com>
2025-08-28 10:12:18 -0400

    s

--- act132/scripts/main.js
+++ act132/scripts/main.js
@@ -2,3 +2,19 @@ console.log("holamundo");
 document.writeln("<h3> HOLA </h3>");
 document.body.style.backgroundColor = '#3498db';
 
+function getDat(form){
+    var formData = new FormData(form);
+
+    for (var pair of formData.entries()){
+        console.log(pair[0] + ": " + pair[1]);
+    }
+
+    console.log(Object.fromEntries(formData));
+    console.log(JSON.stringify(Object.fromEntries(formData), null, 4));
+}
+
+
+document.getElementById("Formulario").addEventListener("submit", function (e){
+    e.preventDefault();
+    getDat(e.target);
+});
