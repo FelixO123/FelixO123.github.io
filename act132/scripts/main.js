943667b800170cf13e36df4d47651eb35013015c
Your Name <you@example.com>
2025-08-28 10:43:39 -0400

    s

--- act132/scripts/main.js
+++ act132/scripts/main.js
@@ -1,20 +1,16 @@
 document.body.style.backgroundColor = '#3498db';
-console.log("PRUEBA ERROR");
+
 
 function getData(form) {
-    try {
-        var formData = new FormData(form);
+    var formData = new FormData(form);
 
-        for (var pair of formData.entries()) {
-            console.log(pair[0] + ": " + pair[1]);
-        }
-
-        console.log(Object.fromEntries(formData));
-        console.log(JSON.stringify(Object.fromEntries(formData), null, 4));
-    } catch (error) {
-        //console.error('Error en get: ', error)
-        console.log("PRUEBA ERROR");
+    for (var pair of formData.entries()) {
+        console.log(pair[0] + ": " + pair[1]);
     }
+
+    console.log(Object.fromEntries(formData));
+    console.log(JSON.stringify(Object.fromEntries(formData), null, 4));
+    console.log("FIN DE GETDAT");
 }
 
 
