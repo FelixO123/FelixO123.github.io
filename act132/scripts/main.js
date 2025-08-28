b72c43a352103c43be6e152ef3e7dd16a91e5919
Your Name <you@example.com>
2025-08-28 13:23:24 -0400

    S

--- act132/scripts/main.js
+++ act132/scripts/main.js
@@ -17,12 +17,75 @@ function getData(form) {
 
 function retornarEdad(e) {
     var formData = new FormData(e.target);
-
-    for(var pair of formData.entries()){
-        
-    }
+    return edad = formData.get("edad");
 }
 
+function retornarNombre(e) {
+    var formData = new FormData(e.target);
+    return nombre = formData.get("nombre_usuario");
+}
+
+function descEdad(e){
+    var edad = retornarEdad(e);
+    var nombre = retornarNombre(e);
+
+    if (edad <= 0) {
+        const message = nombre + ", " + edad + " es una EDAD INVALIDA.";
+        const parrafo = document.createElement("p");
+        parrafo.textContent = message;
+        parrafo.style.color = "white";
+
+        const formulario = document.getElementById("Formulario");
+        formulario.insertAdjacentElement("afterend", parrafo);
+        return edad;
+    } else {
+        if (edad > 0) {
+            if (edad < 18) {
+                const message = nombre + ", tu edad " + edad + "es de PERSONA MENOR DE EDAD.";
+                const parrafo = document.createElement("p");
+                parrafo.textContent = message;
+                parrafo.style.color = "white";
+
+                const formulario = document.getElementById("Formulario");
+                formulario.insertAdjacentElement("afterend", parrafo);
+                return edad;
+
+            } else if (edad < 65) {
+                const message = nombre + ", tu edad " + edad + "es de PERSONA ADULTA.";
+                const parrafo = document.createElement("p");
+                parrafo.textContent = message;
+                parrafo.style.color = "white";
+
+                const formulario = document.getElementById("Formulario");
+                formulario.insertAdjacentElement("afterend", parrafo);
+                return edad;
+
+            } else if (edad < 85) {
+                const message = nombre + ", tu edad " + edad + "es de PERSONA ADULTA MAYOR.";
+                const parrafo = document.createElement("p");
+                parrafo.textContent = message;
+                parrafo.style.color = "white";
+
+                // Insertar el párrafo después del formulario
+                const formulario = document.getElementById("Formulario");
+                formulario.insertAdjacentElement("afterend", parrafo);
+                return Edad;
+
+            } else if (edad >= 85) {
+                const message = nombre + ", tu edad " + edad + "es de PERSONA DE AÑOS DORADOS";
+                const parrafo = document.createElement("p");
+                parrafo.textContent = message;
+                parrafo.style.color = "white"; // Opcional: poner el texto en rojo para destacarlo  
+                // Insertar el párrafo después del formulario
+                const formulario = document.getElementById("Formulario");
+                formulario.insertAdjacentElement("afterend", parrafo);
+                return edad;
+            }
+
+        }
+    }
+        
+}
 
 document.getElementById("Formulario").addEventListener("submit", function (e) {
     console.log("INICIO DOCUMENT");
@@ -33,6 +96,10 @@ document.getElementById("Formulario").addEventListener("submit", function (e) {
     getData(e.target);
     console.log("Fin DOCUMENT");
 
-    retornarEdad(e);
+    descEdad(e);
+
+
+    
+    
     
 });
