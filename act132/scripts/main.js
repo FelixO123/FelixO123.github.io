87816b7fc56ee9d5f17d7df493ea8f44ff26e220
Your Name <you@example.com>
2025-08-28 11:17:13 -0400

    sss

--- act132/scripts/main.js
+++ act132/scripts/main.js
@@ -7,6 +7,7 @@ function getData(form) {
 
     for (var pair of formData.entries()) {
         console.log(pair[0] + ": " + pair[1]);
+
     }
 
     console.log(Object.fromEntries(formData));
@@ -14,10 +15,24 @@ function getData(form) {
     console.log("Fin funcion GET DATA");
 }
 
+function retornarEdad(e) {
+    var formData = new FormData(e.target);
+
+    for(var pair of formData.entries()){
+        console.log(pair[1]);
+    }
+}
+
 
 document.getElementById("Formulario").addEventListener("submit", function (e) {
     console.log("INICIO DOCUMENT");
+    console.log("INICIO PRINT 'e'");
+    console.log(e);
+    console.log("fin PRINT 'e'");
     e.preventDefault();
     getData(e.target);
     console.log("Fin DOCUMENT");
+
+    retornarEdad(e);
+    
 });
