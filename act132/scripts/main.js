f7a396c41bc9a9e6f51d47e7cc693bcbaae4ccb6
Your Name <you@example.com>
2025-08-28 10:37:53 -0400

    s

--- act132/scripts/main.js
+++ act132/scripts/main.js
@@ -1,23 +1,23 @@
 document.body.style.backgroundColor = '#3498db';
 
-function getData(form){
-    try{
-    var formData = new FormData(form);
+function getData(form) {
+    try {
+        var formData = new FormData(form);
 
-    for (var pair of formData.entries()){
-        console.log(pair[0] + ": " + pair[1]);
-    }
+        for (var pair of formData.entries()) {
+            console.log(pair[0] + ": " + pair[1]);
+        }
 
-    console.log(Object.fromEntries(formData));
-    console.log(JSON.stringify(Object.fromEntries(formData), null, 4));
-    }catch(error){
+        console.log(Object.fromEntries(formData));
+        console.log(JSON.stringify(Object.fromEntries(formData), null, 4));
+    } catch (error) {
         //console.error('Error en get: ', error)
         console.log("PRUEBA ERROR");
     }
 }
 
 
-document.getElementById("Formulario").addEventListener("submit", function (e){
+document.getElementById("Formulario").addEventListener("submit", function (e) {
     e.preventDefault();
     getData(e.target);
 });
